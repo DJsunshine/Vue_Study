88ead986f2764483f637df14130ea8735be4f33a
sunshine <1159397792@qq.com>
2017-11-17 17:34:56 +0800

    ok

---
+++ vue/Hello.vue
@@ -0,0 +1,14 @@
+<template>
+	<p>{{greeting}} World!</p>
+</template>
+
+<script src="../js/vue.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+	var app=new Vue({
+		data:function(){
+			return {
+				greeting:'Hello'
+			}
+		}
+	})
+</script>
